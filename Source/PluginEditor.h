c6d5bff23ef512f2f8389e5d8d7d4e6dc7e9f8a0
Nickgonzales76017 <45552585+Nickgonzales76017@users.noreply.github.com>
2021-11-06 17:36:42 -0500

    Update PluginEditor.h

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -29,6 +29,36 @@ public:
 
 private:
     SimpleReverbAudioProcessor& audioProcessor;
+    
+    enum {
+        editorWidth = 500,
+        editorMargin = 10,
+        editorPadding = 10,
+
+        sliderTextEntryBoxWidth = 100,
+        sliderTextEntryBoxHeight = 25,
+        sliderHeight = 25,
+        buttonHeight = 25,
+        comboBoxHeight = 25,
+        labelWidth = 100,
+    };
+
+    //======================================
+
+    OwnedArray<Slider> sliders;
+    OwnedArray<ToggleButton> toggles;
+    OwnedArray<ComboBox> comboBoxes;
+
+    OwnedArray<Label> labels;
+    Array<Component*> components;
+
+    typedef AudioProcessorValueTreeState::SliderAttachment SliderAttachment;
+    typedef AudioProcessorValueTreeState::ButtonAttachment ButtonAttachment;
+    typedef AudioProcessorValueTreeState::ComboBoxAttachment ComboBoxAttachment;
+
+    OwnedArray<SliderAttachment> sliderAttachments;
+    OwnedArray<ButtonAttachment> buttonAttachments;
+    OwnedArray<ComboBoxAttachment> comboBoxAttachments;
 
     NameLabel sizeLabel,
               dampLabel,
